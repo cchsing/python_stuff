dd64c4295c9b82c17469298f2d302153f7096cc1
Justin Chong, Chia-Hsing <chiahsing.chong@outlook.com>
2022-12-01 17:40:44 +0800

    update

---
+++ scripts/README.md
@@ -0,0 +1,13 @@
+# Catalog of the functionality of the scripts
+
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. Conversion of CSV to List: [Script 11](./script11.py)
---
+++ scripts/script11.py
@@ -0,0 +1,18 @@
+## Converting the csv to list 
+
+import csv
+
+class csv_to_list_1:
+
+    def __init__(self, filename, newline, encoding):
+        self.filename = filename
+        self.newline = newline
+        self.encoding = encoding
+
+    
+
+    def convert(self):
+        with open(self.filename, newline=self.newline, encoding=self.encoding) as f:
+            reader = csv.reader(f)
+            data = list(reader)
+        return data
